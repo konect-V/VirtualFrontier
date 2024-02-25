977f6328114991913482fcb0dcd3295331973075
konect-V <66421869+konect-V@users.noreply.github.com>
2024-02-25 09:09:57 +0100

    Update quiet wget

--- init.sh
+++ init.sh
@@ -5,12 +5,12 @@ pip install discord.py TTS huggingface-hub torch langchain langchain-community p
 
 if ! [ -f llm_model_fast.gguf ]; then
     echo "Download gpt4all-falcon-newbpe-q4_0.ggu as llm_model_fast.gguf"
-    wget https://gpt4all.io/models/gguf/gpt4all-falcon-newbpe-q4_0.gguf -O llm_model_fast.gguf
+    wget -q https://gpt4all.io/models/gguf/gpt4all-falcon-newbpe-q4_0.gguf -O llm_model_fast.gguf
 fi 
 
 if ! [ -f llm_model_large.gguf ]; then
     echo "Download wizardlm-13b-v1.2.Q4_0.gguf as llm_model_large.gguf"
-    wget https://gpt4all.io/models/gguf/wizardlm-13b-v1.2.Q4_0.gguf -O llm_model_large.gguf
+    wget -q https://gpt4all.io/models/gguf/wizardlm-13b-v1.2.Q4_0.gguf -O llm_model_large.gguf
 fi
 
 python3 main.py
