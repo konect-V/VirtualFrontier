049d106d7d7f081596ce653741fc50607778b263
konect-V <66421869+konect-V@users.noreply.github.com>
2024-02-27 12:41:02 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,13 +4,13 @@ echo "Downloading pip deps ..."
 pip install discord.py TTS huggingface-hub torch langchain langchain-community pydub gtts langdetect fs llama-cpp-python
 
 if ! [ -f llm_model_fast.gguf ]; then
-    echo "Downloading gpt4all-falcon-newbpe-q4_0.ggu as llm_model_fast.gguf ..."
+    echo "Downloading fast model ..."
     wget -q https://gpt4all.io/models/gguf/gpt4all-falcon-newbpe-q4_0.gguf -O llm_model_fast.gguf
 fi 
 
 if ! [ -f llm_model_large.gguf ]; then
-    echo "Downloading wizardlm-13b-v1.2.Q4_0.gguf as llm_model_large.gguf ..."
-    wget -q https://gpt4all.io/models/gguf/wizardlm-13b-v1.2.Q4_0.gguf -O llm_model_large.gguf
+    echo "Downloading large model ..."
+    wget -q https://gpt4all.io/models/gguf/nous-hermes-llama2-13b.Q4_0.gguf -O llm_model_large.gguf
 fi
 
 if ! [ -d tts_model ]; then
