b9d4b13720c6616ecc26ab57fc0a156a6c7b0e44
konect-V <66421869+konect-V@users.noreply.github.com>
2024-02-29 08:40:18 +0100

    Update : improvements

--- main.py
+++ main.py
@@ -71,35 +71,32 @@ def generate_audio_worker():
         if not os.path.exists(tmpfspath + "/output.wav"):
             output_path = tmpfspath + "/output.wav"
 
-        try:
-            language = detect(voice_str)
-            if use_natural_tts:
-                read_audio_path += ".wav"
-                tts.tts_to_file(text=voice_str, speaker_wav="input.wav", language=language, file_path=output_path)
-                # Concatenate the two wav
-                if output_path != tmpfspath + "/output.wav":
-                    sound_first = AudioSegment.from_wav(tmpfspath + "/output.wav")
-                    sound_second = AudioSegment.from_wav(read_audio_path)
-                    combined_sounds = sound_first + sound_second
-                    combined_sounds.export(tmpfspath + "/output.wav", format="wav")
-                else:
-                    shutil.copyfile(output_path, read_audio_path)
+        language = detect(voice_str)
+        if use_natural_tts:
+            read_audio_path += ".wav"
+            tts.tts_to_file(text=voice_str, speaker_wav="input.wav", language=language, file_path=output_path)
+            # Concatenate the two wav
+            if output_path != tmpfspath + "/output.wav":
+                sound_first = AudioSegment.from_wav(tmpfspath + "/output.wav")
+                sound_second = AudioSegment.from_wav(read_audio_path)
+                combined_sounds = sound_first + sound_second
+                combined_sounds.export(tmpfspath + "/output.wav", format="wav")
             else:
-                # Use gTTS for faster process
-                read_audio_path += ".mp3"
-                gTTS(text=voice_str, lang=language).save(read_audio_path)
-                # Concatenate the two wav
-                if output_path != tmpfspath + "/output.wav":
-                    sound_first = AudioSegment.from_wav(tmpfspath + "/output.wav")
-                    sound_second = AudioSegment.from_mp3(read_audio_path)
-                    combined_sounds = sound_first + sound_second
-                    combined_sounds.export(tmpfspath + "/output.wav", format="wav")
-                else:
-                    AudioSegment.from_mp3(read_audio_path).export(output_path, format="wav")
+                shutil.copyfile(output_path, read_audio_path)
+        else:
+            # Use gTTS for faster process
+            read_audio_path += ".mp3"
+            gTTS(text=voice_str, lang=language).save(read_audio_path)
+            # Concatenate the two wav
+            if output_path != tmpfspath + "/output.wav":
+                sound_first = AudioSegment.from_wav(tmpfspath + "/output.wav")
+                sound_second = AudioSegment.from_mp3(read_audio_path)
+                combined_sounds = sound_first + sound_second
+                combined_sounds.export(tmpfspath + "/output.wav", format="wav")
+            else:
+                AudioSegment.from_mp3(read_audio_path).export(output_path, format="wav")
 
-            audio_generate_index += 1
-        except:
-            pass
+        audio_generate_index += 1
 
         audio_generate_queu.task_done()
 
@@ -353,6 +350,7 @@ async def slash_command(interaction: discord.Interaction):
 @client.event
 async def on_ready():
     await tree.sync()
+    await client.change_presence(activity=discord.Streaming(name="Github", url="https://github.com/konect-V/VirtualFrontier"))
     print(f'Logged in as `{client.user}`')
 
 client.run(os.environ["DISCORD_TOKEN"])
